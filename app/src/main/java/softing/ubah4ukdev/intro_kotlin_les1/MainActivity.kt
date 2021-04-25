cbef710dbaa86aed59388181be8b8abfd796ff04
UBaHbI4 <kinoshnik_ivan@mail.ru>
2021-04-25 22:07:00 +0700

    1. Добавление кнопки в разметку и назначение для неё clickListener

--- app/src/main/java/softing/ubah4ukdev/intro_kotlin_les1/MainActivity.kt
+++ app/src/main/java/softing/ubah4ukdev/intro_kotlin_les1/MainActivity.kt
@@ -1,11 +1,19 @@
 package softing.ubah4ukdev.intro_kotlin_les1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_show_property: MaterialButton
+        btn_show_property = findViewById(R.id.btn_show_properties)
+        btn_show_property?.setOnClickListener({
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+        })
     }
 }
